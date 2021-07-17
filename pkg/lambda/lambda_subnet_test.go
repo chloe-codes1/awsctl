ba41a96d837bcb710b76aefabd084d0fc3951904
chloe-codes1 <juhyun.kim@lindsey.edu>
2021-07-18 02:52:36 +0900

    Add lambda subnet test cases

---
+++ pkg/lambda/lambda_subnet_test.go
@@ -0,0 +1,32 @@
+package lambda
+
+import (
+	"github.com/stretchr/testify/suite"
+	"lambda-manager/internal/config"
+	"strings"
+	"testing"
+)
+
+type LambdaSubnetSuite struct {
+	suite.Suite
+}
+
+func TestLambdaSubnet(t *testing.T){
+	lambdaSubnetSuite:= new(LambdaSubnetSuite)
+	suite.Run(t, lambdaSubnetSuite)
+}
+
+func (suite *LambdaSubnetSuite) SetupSuite() {
+	configFile := strings.Join([]string{"internal/config/config.devel.yml"}, "")
+
+	conf, err := config.ReadConfig(configFile)
+	if err != nil {
+		panic(err)
+	}
+	config.VPCConf = &conf.VPC
+	config.SubnetConf = &conf.Subnet
+}
+
+func (suite *LambdaSubnetSuite) TestGetLambdaWithVPC() {
+	GetLambdaWithVpc()
+}
